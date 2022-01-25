5c8fcd8a42b687e0f223dbd1c44e4932527639ab
Matias Schwinch <matias.schwinch@outlook.com>
2022-01-25 17:51:14 -0300

    Ahora compatible con SQL Server.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using GeneradorBaseDatos.Model;
 using GeneradorBaseDatos.Service;
 using System;
-using System.Collections;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -13,67 +12,91 @@ namespace GeneradorBaseDatos
     {
         static async Task Main(string[] args)
         {
-            Console.Title = "Generador de bases de datos: SQLite";
+            Console.Title = "PersonDBGenerator: SQL Server & SQLite";
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-            int QuantityToAdd;
-            string ReadQuantity;
-            bool ExitLoop = false;
+            int QuantityToAdd = 0;
             int i = 0;
 
-            Console.WriteLine(FiggleFonts.Small.Render("Generador de bases de datos"));
+            Console.WriteLine(FiggleFonts.Stop.Render("PersonDBGenerator"));
             Console.WriteLine("Autor: Matias Schwinch");
             Console.WriteLine("Repositorio: https://github.com/MatiasSchwinch");
             Console.WriteLine("API utilizada: https://randomuser.me/\n");
 
+            //  Ingresar la cantidad de registros a generar.
             Console.WriteLine("Ingrese la cantidad de registros (máximo soportado por la API: 5000) que desea añadir a la base de datos:");
-
-            do
-            {
-                ReadQuantity = Console.ReadLine();
-
-                if (!int.TryParse(ReadQuantity, out QuantityToAdd)) { Console.WriteLine($"\"{ReadQuantity}\" no es un número, ingrese un numero valido."); continue; }
-                if (QuantityToAdd < 1 || QuantityToAdd > 5000) { Console.WriteLine("El mínimo de registros que se pueden generar es 1, y el máximo es 5000."); continue; }
-
-                ExitLoop = true;
-
-            } while (!ExitLoop);
+            RequestUserNumber(1, 5000, ref QuantityToAdd, "El mínimo de registros que se pueden generar es {0}, y el máximo es {1}.");
 
             Console.WriteLine("Conectando con la API...");
 
             //  Conexión con la API.
-            Connection<API> connection = new("https://randomuser.me/api/");
+            Connection<API> connection = new("https://randomuser.me/api/", "?results={0}");
             API dataReceived = await connection.GetDeserializeData(QuantityToAdd);
-            if (dataReceived is null) { Environment.Exit(1); }
+            if (dataReceived is null) { Console.WriteLine("No se han recibido datos de la API."); Environment.Exit(1); }
 
             Console.WriteLine("Registros recibidos...");
 
-            using DBConnector dB = new();
+            //  Elegir que gestor de bases de datos utilizar.
+            Console.WriteLine("\nEn que gestor de bases de datos desea guardar la información recibida?:\n" +
+                "1: SQL Server (debe tener configurada previamente la cadena de conexión en el archivo \"config.json\")\n" +
+                "2: SQLite");
+            RequestUserNumber(1, 2, ref QuantityToAdd, "Solo puede elegir entre 2 opciones.");
 
-            foreach (var item in dataReceived.Results)
-            {
-                Person person = new();
-                person = item;
-                person.FirstName = item.Name.FirstName;
-                person.LastName = item.Name.LastName;
-                person.Date = item.Dob.Date;
-                person.Age = item.Dob.Age;
-
-                dB.Person.Add(person);
-                Console.WriteLine($"Registro listado no.:{++i} - {person}");
-            }
-
-            //  Guardado de los registros en la Base de datos.
             try
             {
+                using DBConnector dB = new((DBConnector.DatabaseEngine)QuantityToAdd);
+
+                foreach (var item in dataReceived.Results)
+                {
+                    Person person = new();
+                    person = item;
+                    person.FirstName = item.Name.FirstName;
+                    person.LastName = item.Name.LastName;
+                    person.Date = item.Dob.Date;
+                    person.Age = item.Dob.Age;
+
+                    dB.Person.Add(person);
+                    Console.WriteLine($"Registro listado no. {++i}: {person}");
+                }
+
+                Console.WriteLine("Guardando registros...");
+
+                //  Guardado de los registros en la Base de datos.
                 dB.SaveChanges();
-                Console.WriteLine("Los registros se añadieron a la base de datos correctamente.");
+
+                Console.WriteLine("Los registros se añadieron a la base de datos en {0} correctamente.", (DBConnector.DatabaseEngine)QuantityToAdd);
             }
             catch (Exception e)
             {
                 Console.WriteLine($"No se pudieron añadir los registros debido a un error: {e.Message}");
             }
+
+            //  Salida del programa.
+            Console.WriteLine("\nPresione una tecla para salir...");
+            Console.ReadKey();
             
+        }
+
+        /// <summary>
+        ///     Pide al usuario que ingrese un numero entre un rango preestablecido, valida y transforma lo que el usuario ingresa y devuelve el resultado en una variable que debe ser pasada como referencia.
+        /// </summary>
+        /// <param name="pMin">El valor mínimo que puede ser aceptado.</param>
+        /// <param name="pMax">El valor máximo que puede ser aceptado.</param>
+        /// <param name="pStoreNum">Una variable que debe ser pasada como referencia, aquí es donde se va a almacenar el numero en el caso que este sea valido.</param>
+        /// <param name="pValidationExceedMaxMSG">Mensaje que el usuario va a ver como feedback si excede el máximo, puede usar {0} y {1} para que se reemplacen por el numero mínimo y máximo aceptados respectivamente.</param>
+        static void RequestUserNumber(int pMin, int pMax, ref int pStoreNum, string pValidationExceedMaxMSG)
+        {
+            bool endLoop = false;
+            do
+            {
+                var ReadQuantity = Console.ReadLine();
+
+                if (!int.TryParse(ReadQuantity, out pStoreNum)) { Console.WriteLine("\"{0}\" no es un número, ingrese un numero valido.", ReadQuantity); continue; }
+                if (pStoreNum < pMin || pStoreNum > pMax) { Console.WriteLine(pValidationExceedMaxMSG, pMin, pMax); continue; }
+
+                endLoop = true;
+
+            } while (!endLoop);
         }
     }
 
@@ -81,17 +104,29 @@ namespace GeneradorBaseDatos
     {
         private static HttpClient _client = new();
         private readonly string _apiURL;
+        private readonly string _apiEndpoint;
 
-        public Connection(string pURL)
+        /// <summary>
+        ///     Capaz de conectarse con servicios a través de solicitudes HTTP, esta enfocado principalmente a servicios de generación de perfiles ficticios.
+        /// </summary>
+        /// <param name="pURL">La URL del servicio, esta debe terminar en "/"</param>
+        /// <param name="pEndpoint">El endpoint que nos va a proveer la información, este debe contener un parámetro numérico, que debe ser reemplazo por "{0}". EJ: ?results=5 debe quedar ?results={0}</param>
+        public Connection(string pURL, string pEndpoint)
         {
             _apiURL = pURL;
+            _apiEndpoint = pEndpoint;
         }
 
+        /// <summary>
+        ///     Conecta con la API, y deserializa la información obtenida por la misma.
+        /// </summary>
+        /// <param name="pQuantityToGenerate">La cantidad de registros que se quieran obtener, este parámetro reemplaza el "{0}" del endpoint</param>
+        /// <returns></returns>
         public async Task<T> GetDeserializeData(int pQuantityToGenerate = 10)
         {
             try
             {
-                string _responseBody = await _client.GetStringAsync($"{_apiURL}?results={pQuantityToGenerate}");
+                string _responseBody = await _client.GetStringAsync($"{_apiURL}{string.Format(_apiEndpoint, pQuantityToGenerate)}");
                 T _deserialize = JsonSerializer.Deserialize<T>(_responseBody);
 
                 return _deserialize;
--- Service/DBConnector.cs
+++ Service/DBConnector.cs
@@ -1,28 +1,71 @@
 ﻿using GeneradorBaseDatos.Model;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace GeneradorBaseDatos.Service
 {
     public class DBConnector : DbContext
     {
         public DbSet<Person> Person {get; set;}
-        
-        private const string DBFileName = "Person.db";
+        private const string _dbTableName = "PersonDBGenerator";
 
-        public DBConnector()
+        private readonly DatabaseEngine _dbEngine;
+        private readonly string _dbSQLConnectionString;
+
+        public DBConnector(DatabaseEngine dbEngine)
         {
+            _dbEngine = dbEngine;
+
+            if (dbEngine is DatabaseEngine.SQLServer)
+            {
+                string obtainConnectionString = JsonSerializer.Deserialize<ConnectionString>(File.Exists("config.json")
+                    ? File.ReadAllText("config.json")
+                    : throw new Exception("El archivo \"config.json\" no se encuentra en el directorio.")).DefaultConnection;
+
+                _dbSQLConnectionString = string.IsNullOrWhiteSpace(obtainConnectionString)
+                    ? throw new Exception("Debe ingresar la cadena de conexión de SQL Server en el archivo \"config.json\" dentro del apartado: DefaultConnection.")
+                    : obtainConnectionString;
+            }
+
             Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite(connectionString: $"Filename={DBFileName}", sqliteOptionsAction: options =>
+            switch (_dbEngine)
             {
-                _ = options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
-            });
+                case DatabaseEngine.SQLServer:
+                    optionsBuilder.UseSqlServer(connectionString: _dbSQLConnectionString, sqlServerOptionsAction: options =>
+                    {
+                        _ = options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+                    });
+                    break;
+
+                case DatabaseEngine.SQLite:
+                    optionsBuilder.UseSqlite(connectionString: $"Filename={_dbTableName}.db", sqliteOptionsAction: options =>
+                    {
+                        _ = options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+                    });
+                    break;
+            }
             
             base.OnConfiguring(optionsBuilder);
+        }
+
+        internal class ConnectionString
+        {
+            [JsonPropertyName("DefaultConnection")]
+            public string DefaultConnection { get; set; }
+        }
+
+        public enum DatabaseEngine
+        {
+            SQLServer = 1,
+            SQLite = 2
         }
     }
 }
